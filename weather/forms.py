95cfdd421750166f8b67aad76bca6a7f4dc359c9
Vladislav <denckic@gmail.com>
2021-03-27 23:03:04 +0200

    Легкие изменения визуала

--- weather/forms.py
+++ weather/forms.py
@@ -5,7 +5,6 @@ class CityForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(CityForm, self).__init__(*args, **kwargs)
         self.fields['city'].label = 'Введите название города'
-
         self.fields['city'].widget.attrs.update({'class': 'form-control-lg d-flex justify-content-center'})
 
     city = forms.CharField(max_length=100)
